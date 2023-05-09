9d3191aa4859815d08f4fc74e9d170d7d4009270
ShatterAr <sascha.s.89@gmx.de>
2023-05-09 12:14:20 +0200

    Variabeln bsp. 1

--- script_01.js
+++ script_01.js
@@ -35,11 +35,35 @@
 
 /* JS ist eine untypisierte Sprache! | untyped */
 
-let test;
-test = "Max"; // string
-test = 2; // number
-test = true; // boolean
+// let test;
+// test = "Max"; // string
+// test = 2; // number
+// test = true; // boolean
 
-console.log("Datentyp: " + typeof test);
-console.log("Inhalt: " + test);
+// console.log("Datentyp: " + typeof test);
+// console.log("Inhalt: " + test);
+
+/***** 03a Konstanten *******/
+
+// let test = "Max"; // Deklaration + Wertzuweisung
+// test = "Maxine";
+
+// const test = "Max"; //Variable mit KONSTANTEM Inhalt
+// test = "Maxine"; // KEINE neue Zuweisung zur LZ möglich! --> Fehler
+
+// console.log("Inhalt: " + test);
+
+/***** 04 Beispiel - Berechnung *******/
+
+//Deklaration
+const birthYearJohn = 2000;
+const birthYearMark = 1990;
+let ageJohn, ageMark;
+
+ageJohn = 2023 - birthYearJohn;
+ageMark = 2023 - birthYearMark;
+
+// Ausgabe
+console.log("ageJohn: " + ageJohn);
+console.log("ageMark: " + ageMark);
 
