1b3c4893172bd832dc34ab00a126fc575fa4de0e
ShatterAr <sascha.s.89@gmx.de>
2023-05-09 11:32:45 +0200

    Variablen Update

--- script_01.js
+++ script_01.js
@@ -10,8 +10,36 @@
 
 // Konsolen-Ausgaben
 // console.log("Hi");
-// console.log(firstName);
+// console.log("firstName");
 
 
 /***** 02 Deklaration + Wertzuweisung I *******/
+
+// let firstname; // Deklaration (Definition)
+// firstname = "Max"; //Wertzuweisung
+// console.log(firstname); // Ausgabe
+
+// let familyName;
+// familyName = "Mütze";
+// console.log(familyName);
+
+/***** 03 Deklaration + Wertzuweisung II *******/
+
+// let firstName, familyName;
+
+// firstName = prompt("Bitte Vornamen eingeben:");
+// familyName = prompt("Bitte Nachnamen eingeben");
+
+// console.log(firstName + " " + familyName);
+// console.log(typeof firstName);
+
+/* JS ist eine untypisierte Sprache! | untyped */
+
+let test;
+test = "Max"; // string
+test = 2; // number
+test = true; // boolean
+
+console.log("Datentyp: " + typeof test);
+console.log("Inhalt: " + test);
 
